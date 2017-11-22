2ba93235452164b36d92d76c5c6100abf1cd1115
SumeshPS <sumesh.sukumaran@gmail.com>
2017-11-22 16:21:16 -0400

    Update countdownrecursion.py

--- countdownrecursion.py
+++ countdownrecursion.py
@@ -4,5 +4,12 @@ def countdown(number):
     else:
         print (number)
         countdown(number-1)
+def sum_number(num):
+    if num ==0:
+        return 0
+    else:
+        return num + sum_number (num-1)
+
           
 countdown(6)
+print(sum_number(4))
